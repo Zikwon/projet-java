2505aaa840dadcd5911747148f080ed1b803689f
Agnès <lucas.agnes@viacesi.fr>
2019-05-31 22:38:43 +0200

    Physic Rock

--- entity/src/main/java/entity/Rock.java
+++ entity/src/main/java/entity/Rock.java
@@ -1,5 +1,55 @@
 package entity;
 
-public class Rock {
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
+public class Rock extends Object {
+
+    private Image image;
+    
+    //private final String PATH_IMAGE = ;
+
+    /*public Rock() {
+		notify.controleur();
+	}*/
+    
+     public Rock(int x, int y) throws IOException {
+        this.x = x;
+        this.y = y;
+        this.image = ImageIO.read(new File("../entity/Images/rock.png"));
+    }
+
+    public Rock(int x, int y, String pathImage) throws IOException {
+        this.x = x;
+        this.y = y;
+        this.image = ImageIO.read(new File(pathImage));
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+    
+    
 }
