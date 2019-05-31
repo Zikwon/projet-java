43d7aed41357a2e4d2e7a42fc4427188d570f454
Gaillan <lucas.gailland@viacesi.fr>
2019-05-31 22:37:25 +0200

    Wall entity

--- entity/src/main/java/entity/Wall.java
+++ entity/src/main/java/entity/Wall.java
@@ -1,5 +1,62 @@
 package entity;
 
-public class Wall {
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
+
+/**
+ * 
+ * @author Lucas Gaillant
+ *
+ */
+
+public class Wall extends Object {
+
+    private Image image;
+    
+    //private final String PATH_IMAGE = ;
+
+    /*public Rock() {
+		notify.controleur();
+	}*/
+    
+     public Wall(int x, int y) throws IOException {
+        this.x = x;
+        this.y = y;
+        this.image = ImageIO.read(new File("../entity/Images/wall.png"));
+    }
+
+    public Wall(int x, int y, String pathImage) throws IOException {
+        this.x = x;
+        this.y = y;
+        this.image = ImageIO.read(new File(pathImage));
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+    
+    
 }
