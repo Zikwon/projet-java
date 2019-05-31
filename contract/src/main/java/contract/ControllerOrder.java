af8f2746617c437544a25defca8631108ff9a7ca
Pasquet <tristan.pasquet@viacesi.fr>
2019-05-31 21:57:22 +0200

    ControllerOrder

--- contract/src/main/java/contract/ControllerOrder.java
+++ contract/src/main/java/contract/ControllerOrder.java
@@ -7,12 +7,14 @@ package contract;
  */
 public enum ControllerOrder {
 
-	/** The English. */
-	English,
-	/** The French. */
-	Francais,
-	/** The Deutch. */
-	Deutsch,
-	/** The Indonesia. */
-	Indonesia
+	
+	map,
+	
+	keyup,
+	
+	keyleft,
+	
+	keydown,
+	
+	keyright,
 }
